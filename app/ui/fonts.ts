3af6456022daecb0cb3448f082794366f672802a
Sissoko20 <m.sissoko.7035@gmail.com>
2024-12-15 15:18:38 +0000

    Initialisation

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import {Inter, Lusitana, Playwrite_VN} from 'next/font/google';
+
+export const inter =Inter({subsets:['latin']})
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    display: 'optional',
+    weight:'400'
+  })
+  export const vn= Playwrite_VN({
+    display:'swap',
+    weight:'400',
+    
+
+  })
